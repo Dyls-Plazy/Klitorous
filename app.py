e33cbb046c2bbd82cecb40bff9e854d6c6c2031b
TriangularDev <72287094+TriangularDev@users.noreply.github.com>
2026-05-31 20:36:02 -0400

    Add listing and setting nodes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import asyncio
 import keldb
+import time
 
 database = keldb.KelDB(keldb.FileStoreHook("./testdb/"))
 
 async def main():
+    await database.set_value(f"Test {time.time()}")
+
     print(await database.get_value())
+
+    async for subnode in database.list_subnodes():
+        print(subnode.path)
 
 asyncio.run(main())
--- keldb.py
+++ keldb.py
@@ -1,31 +1,45 @@
+from typing import Any
 from enum import Enum
 import aiofiles
 import asyncio
+import base64
 import json
 import os
 
-class Enums():
-    class InformationAvailability(Enum):
-        NONE = 1
-        INPUT = 2
-        VALIDATED = 3
-        FULL = 4
-
 class Node():
     def __init__(self):
-        self.InformationAvailability = Enums.InformationAvailability.NONE
-
-        # Basic information (INPUT and VALIDATED information availability)
+        # Basic information
         self.root = None
         self.parent = None
         self.name = None
         self.path = None
 
-    async def get_value(self):
+    async def get_value(self) -> Any:
         return await self.root.hook.get_path_value(self.path)
+    
+    async def set_value(self, value:Any) -> None:
+        await self.root.hook.set_path_value(self.path, value)
+
+    async def list_subnodes(self) -> iter:
+        async for subnode_name in self.root.hook.list_path_subpaths(self.path):
+            subnode = Node()
+
+            subnode.root = self.root
+            subnode.parent = self
+            subnode.name = subnode_name
+            subnode.path = self.path + f"{subnode_name}/"
+
+            yield subnode
 
 class Hook():
-    pass
+    async def get_path_value(self, path:str, cached=False) -> Any:
+        raise NotImplementedError
+    
+    async def set_path_value(self, path:str, value:Any, cached=False) -> Any:
+        raise NotImplementedError
+    
+    async def list_path_subpaths(self, path:str, cached=False) -> iter:
+        raise NotImplementedError
 
 class FileStoreHook(Hook):
     def __init__(self, dir:str):
@@ -33,17 +47,34 @@ class FileStoreHook(Hook):
 
         self.store = {}
     
-    async def get_path_value(self, path:str, cached=False):
-        parts = (self.dir,) + tuple(x for x in path.split("/") if x != "") + ("value.json",)
+    async def get_path_directory(self, path:str, file:str="") -> str:
+        parts = (self.dir,) + tuple(base64.urlsafe_b64encode(x.encode()).decode() for x in path.split("/") if x != "") + (file,)
 
+        return os.path.join(*parts)
+
+    async def get_path_value(self, path:str, cached=False) -> Any:
         if not cached:
-            async with aiofiles.open(os.path.join(*parts), 'r') as f:
+            async with aiofiles.open(await self.get_path_directory(path, "value.json"), 'r') as f:
                 return json.loads(await f.read())
+            
+    async def set_path_value(self, path:str, value:Any, cached=False) -> None:
+        os.makedirs(await self.get_path_directory(path), exist_ok=True)
+
+        async with aiofiles.open(await self.get_path_directory(path, "value.json"), 'w') as f:
+            return await f.write(json.dumps(value))
+        
+    async def list_path_subpaths(self, path:str, cached=False) -> iter:
+        with os.scandir(await self.get_path_directory(path)) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    try:
+                        entry_name = base64.urlsafe_b64decode(entry.name.encode()).decode()
+                    except:continue
+
+                    yield entry_name
 
 class KelDB(Node):
     def __init__(self, hook:Hook):
-        self.InformationAvailability = Enums.InformationAvailability.NONE
-
         self.root = self
         self.parent = None
         self.name = ""
